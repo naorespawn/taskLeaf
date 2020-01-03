9e1a08e2b093b128393f9441a4f2c8ea2895f67c
Naoki Egawa <nao.respawn@gmail.com>
2020-01-03 23:10:24 +0900

    ログイン中のユーザーを特定する current_userメソッドを定義

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  private
+    def current_user
+      # `||=` nilならば実行される
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
 end
