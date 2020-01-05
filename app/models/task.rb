f41b8372cbe19da3013deb39e9a822cf7eb66e00
Naoki Egawa <nao.respawn@gmail.com>
2020-01-05 20:29:42 +0900

    tasks_controller内の重複処理をフィルタ化

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,8 +2,10 @@ class Task < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validate  :validate_name_not_including_comma
   before_validation :set_nameless_name
-  
+
   belong_to :user
+
+  scope :recent, -> { order(created_at: :desc) }
 
   private
     def validate_name_not_including_comma
