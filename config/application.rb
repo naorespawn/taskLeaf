e1a406db354081727a3e42135cc1117b7ce201f9
Naoki Egawa <nao.respawn@gmail.com>
2020-01-12 20:50:42 +0900

    タイムゾーンをAsia/Tokyoに変更

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Taskleaf
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Asia/Tokyo'
 
     # config.time_zone = 'Tokyo'
     # config.active_recode.default_timezone = :local
