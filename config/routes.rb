470fbf9dde7bf431413d90b493d3f7fbd8ebba42
Naoki Egawa <nao.respawn@gmail.com>
2020-01-17 21:32:18 +0900

    新規タスク登録時に確認を挟むアクションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   root to: 'tasks#index'
+  resources :tasks do
+    post :confirm, action: :confirm_new, on: :new
+  end
+
 
   namespace :admin do
     resources :users
