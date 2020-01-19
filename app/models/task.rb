664bee384a35dc7a937358451fa01c02981612e4
Naoki Egawa <nao.respawn@gmail.com>
2020-01-19 21:58:38 +0900

    Ransackの検索条件を指定

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,6 +5,16 @@ class Task < ApplicationRecord
   belongs_to :user
   scope :recent, -> { order(created_at: :desc) }
 
+  # 許可するカラムの指定
+  def self.ransackable_attributes(auth_object = nil)
+    %w[name created_at]
+  end
+
+  # 検索条件に含める関連の指定
+  def self.ransackable_associations(auth_object = nil)
+    []
+  end
+
   private
     def validate_name_not_including_comma
       errors.add(:name, 'にカンマを含めることはできません') if name&.include?(',')
